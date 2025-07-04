5ad02da25c0be76eb13eaf503b18205c9c8e926f
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-04 19:04:54 +0530

    started programming my server

--- Day2/server.js
+++ Day2/server.js
@@ -2,6 +2,15 @@ const express = require("express")
 
 const app = express();
 
+
+app.get('/home',(req,res)=>{
+    res.send("Welcome to the Home page")
+})
+
+app.get('/about',(req,res)=>{
+    res.send("Welcome to the about page")
+})
+
 app.listen(3000,()=>{
     console.log("server is runnuing on the post 3000");
     
