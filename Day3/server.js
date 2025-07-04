4dad5049941283e6fd2a2ea503a7d3d4744c7e2a
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-04 19:33:03 +0530

    started programming my server and added the notes sending functionality

--- Day3/server.js
+++ Day3/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 
-const app = express()
+const app = express();
+
+
+// Using n=middle where to read the data send form the frontend
+
+app.use(express.json())
+
+// Notes APi
+app.post('/notes',(req,res)=>{
+    console.log(req.body);
+    
+})
 
 app.listen(3000,(req,res)=>{
     console.log('Server is running on port 3000')
