8968c2e91d84bcb366932d3d73e9eff26d3e4177
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-08 20:05:41 +0530

    programming the server

--- Day4/server.js
+++ Day4/server.js
@@ -2,6 +2,22 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello world");
+})
+
+
+app.post('/notes',(req,res)=>{
+    const {title,content} = req.body;
+    console.log(title,content);
+    
+    
+    
+
+})
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
