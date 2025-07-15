d399ec81f26266dd4497305c706160721578d1cf
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-15 19:41:22 +0530

    doing some structing

---
+++ Day5_Project/frontend/src/components/FaceMoodDetector.jsx
@@ -0,0 +1,60 @@
+import React, { useRef, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
+
+const FaceMoodDetector = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    const loadModels = async () => {
+      await Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      ]);
+      startVideo();
+    };
+
+    const startVideo = () => {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+        })
+        .catch((err) => console.error('Error accessing webcam:', err));
+    };
+
+    loadModels();
+  }, []);
+
+  const onPlay = () => {
+    const interval = setInterval(async () => {
+      if (!videoRef.current) return;
+
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceExpressions();
+
+      if (detections.length > 0) {
+        const expressions = detections[0].expressions;
+        const mood = Object.entries(expressions).reduce((a, b) => a[1] > b[1] ? a : b)[0];
+        console.log('Current Mood:', mood);
+      }
+    }, 300);
+
+    return () => clearInterval(interval);
+  };
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        width="720"
+        height="560"
+        onPlay={onPlay}
+        style={{ border: '2px solid #444', borderRadius: '10px' }}
+      />
+    </div>
+  );
+};
+
+export default FaceMoodDetector;
