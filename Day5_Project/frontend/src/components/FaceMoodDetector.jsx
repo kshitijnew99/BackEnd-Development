50d251612ca0864e383d19cc815d044548e0485e
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-18 18:35:32 +0530

    changes few of them

--- Day5_Project/frontend/src/components/FaceMoodDetector.jsx
+++ Day5_Project/frontend/src/components/FaceMoodDetector.jsx
@@ -4,60 +4,62 @@ import * as faceapi from 'face-api.js';
 const FaceMoodDetector = () => {
   const videoRef = useRef();
 
-  useEffect(() => {
-    const loadModels = async () => {
-      await Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-      ]);
-      startVideo();
-    };
-
-    const startVideo = () => {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then((stream) => {
-          videoRef.current.srcObject = stream;
-        })
-        .catch((err) => console.error('Error accessing webcam:', err));
-    };
-
-    loadModels();
-  }, []);
-
-  const onPlay = () => {
-    const interval = setInterval(async () => {
-      if (!videoRef.current) return;
-
-      const detections = await faceapi
-        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
-        .withFaceExpressions();
-
-      if (detections.length > 0) {
-        const expressions = detections[0].expressions;
-        const mood = Object.entries(expressions).reduce((a, b) => a[1] > b[1] ? a : b)[0];
-        console.log('Current Mood:', mood);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
+  const loadModels = async () => {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    ]);
+        
   };
 
-  // const detecthandler = () => {
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((err) => console.error('Error accessing webcam:', err));
+  };
 
-  // }
+  const detectHandler = async () => {
+    if (!videoRef.current) return;
+
+    const detections = await faceapi
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
+
+    if(!detections || detections.length === 0){
+      console.log("No face detected");
+    }
+
+    if (detections.length > 0) {
+      const expressions = detections[0].expressions;
+      const mood = Object.entries(expressions).reduce((a, b) => a[1] > b[1] ? a : b)[0];
+      console.log('Current Mood:', mood);
+    }
+  };
+
+  useEffect(() => {
+
+      loadModels().then(startVideo); 
+
+  }, []);
+
+    
+
+  
 
   return (
-    <div className='facemood'>
-      <video 
-      className='video' 
-      ref={videoRef} 
-      autoPlay 
-      muted 
-      onPlay={onPlay} />
-      <button   className='detect'>Detect</button>
+      <div className='facemood'>
+        <video 
+        className='video' 
+        ref={videoRef} 
+        autoPlay 
+        muted 
+        />
+        <button  onClick={detectHandler} className='detect'>Detect</button>
 
-    </div>
-  );
+      </div>
+    );
 };
 
 export default FaceMoodDetector;
