6b2ad69a25a6be1cf93a717d6e92989845b9295d
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-27 21:00:07 +0530

    pushning our data through postman(front-end)

--- Day5_Project/backend/src/routes/song.routes.js
+++ Day5_Project/backend/src/routes/song.routes.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const multer = require('multer')
-
-
 const router = express.Router()
 
-
-
-
-const upload = multer({storage:multer.memoryStorage()}); // this is a middleware, just like "app.use(express.json())". memoryStorage is RAM of Server
+const upload = multer({storage:multer.memoryStorage()}); 
+/*
+Multer - this is a middleware, just like "app.use(express.json())". memoryStorage is RAM of Server
+*/ 
 
 /*
 title
@@ -17,9 +15,11 @@ audio
 */
 
 
-router.post('/song',(req,res)=>{
+router.post('/song',upload.single("audio"),(req,res)=>{
     
     console.log(req.body);
+    console.log(req.file);
+    
     res.status(201).json({
         message:"Song created successfully",
         song:req.body
