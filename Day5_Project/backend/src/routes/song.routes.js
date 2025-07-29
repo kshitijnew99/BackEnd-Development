af485222621ea03b46b74c04d03e434c3f7c0f3b
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-30 02:12:59 +0530

    creating sing models

--- Day5_Project/backend/src/routes/song.routes.js
+++ Day5_Project/backend/src/routes/song.routes.js
@@ -2,18 +2,17 @@ const express = require('express')
 const multer = require('multer')
 const router = express.Router()
 const uploadFile = require('../service/storage.service')
-const upload = multer({storage:multer.memoryStorage()}); 
-/*
-Multer - this is a middleware, just like "app.use(express.json())". memoryStorage is RAM of Server
-*/ 
+const upload = multer({storage:multer.memoryStorage()});
+const songModel = require('../models/song.model')
 
 /*
+Multer - this is a middleware, just like "app.use(express.json())". memoryStorage is RAM of Server
+
 title
 artist
 audio
-
 */
-
+ 
 
 router.post('/song', upload.single("audio"), async (req, res) => {
     try {
@@ -23,17 +22,24 @@ router.post('/song', upload.single("audio"), async (req, res) => {
         const fileData = await uploadFile(req.file);
         console.log(fileData);
 
+        const song = await songModel.create({
+            title:req.body.title,
+            artist:req.body.artist,
+            audio:fileData.url,
+            mood:req.body.mood
+        })
+
+
+
         res.status(201).json({
             message: "Song created successfully",
-            song: req.body,
-            fileURL: fileData.url // Optional, but useful
+            song: song
+            
         });
     } catch (error) {
         console.error("Upload failed:", error);
         res.status(500).json({ message: "Upload failed", error });
     }
 });
-
-
 
 module.exports = router;
