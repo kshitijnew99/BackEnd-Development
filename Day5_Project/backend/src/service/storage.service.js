e0df63d0eadfc76de88a2f3db5683c369e7f20e1
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-29 21:44:30 +0530

    setting up storage

---
+++ Day5_Project/backend/src/service/storage.service.js
@@ -0,0 +1,10 @@
+/*in this we put the code of the storage provider, and if 
+someone give better price then we move to their service*/  
+
+var ImageKit = require("imagekit");
+
+var imagekit = new ImageKit({
+    publicKey : process.env.imagekit_publicKey,
+    privateKey : process.env.imagekit_privateKey,
+    urlEndpoint : process.env.imagekit_URL
+});
