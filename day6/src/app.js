6d3f607d4b6d30fbaa44ba70ad518c5a554df666
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-31 23:34:22 +0530

    adding middleware

--- day6/src/app.js
+++ day6/src/app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json())
 
-
+app.use((req,res,next)=>{
+    console.log("this middleware is btw app and routes");
+    next(); // request will not procide id you not add next()
+})
 
 app.use('/',indexRoutes)
 
