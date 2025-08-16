2fc77387a35d5fdd6ecc7f6efb08ec5168ed5214
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-08-16 16:02:33 +0530

    build custom events

--- day7/server.js
+++ day7/server.js
@@ -6,11 +6,20 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
-io.on("connection", (socket) => {
+io.on("connection", (socket) => { // connection is an event
   // ...
-//   console.log('Socket.io connected');
+  console.log('Socket.io connected');
 
-  
+  socket.on("disconnect",()=>{
+    console.log('Socket.io disconnected'); // disconnection is an event
+  })
+
+  socket.on("message",(data)=>{
+    console.log('this is message event in socket iO');
+    console.log(data);
+    
+    
+  })
 });
 
 httpServer .listen(3000,()=>{
