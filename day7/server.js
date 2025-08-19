1c620b886422e1f9ed9a4f082cb7efdb6a085dd0
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-08-19 23:41:47 +0530

    some changes

--- day7/server.js
+++ day7/server.js
@@ -2,27 +2,27 @@ require('dotenv').config()
 const app = require('./src/app')
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateResponse = require('./src/service/ai.service')
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
-io.on("connection", (socket) => { // connection is an event
-  // ...
+io.on("connection", (socket) => { 
   console.log('Socket.io connected');
 
   socket.on("disconnect",()=>{
-    console.log('Socket.io disconnected'); // disconnection is an event
+    console.log('Socket.io disconnected');  
   })
 
-  socket.on("message",(data)=>{
-    console.log('this is message event in socket iO');
-    console.log(data);
-    
-    
+  socket.on("Question",async (data)=>{
+    console.log("Received AI message");
+    const response = await generateResponse(data.prompt)
+    console.log("AI response : ",response);
+    socket.emit("Answer", {response})
+
   })
+  
 });
-
 httpServer .listen(3000,()=>{
     console.log("server is running on port 3000");
-    
 })
--- day7/src/service/ai.service.js
+++ day7/src/service/ai.service.js
@@ -1,13 +1,19 @@
-const { GoogleGenAI } = require("@google/genai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
+async function generateResponse(prompt) {
+    const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
+    
+    const result = await model.generateContent({
+        contents: [
+            {
+                role: "user",
+                parts: [{ text: prompt }]
+            }
+        ]
+    });
+    return result.response.text();
 }
 
-await main();
+module.exports = generateResponse;
