af33db2d8e6c1b84ae9661c3936c7b28f74748d4
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-08-25 04:05:31 +0530

    changes DB.js

--- day6/src/db/db.js
+++ day6/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connectDB(){
-    mongoose.connect(process.env.mongoDB_url)
+    mongoose.connect(process.env.mongoDB_url )
     .then(()=>{
         console.log('connected to DB');
         
