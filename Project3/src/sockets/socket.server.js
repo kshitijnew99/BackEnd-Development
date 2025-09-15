f0b790e11d7aaa1e8694217afaaf69deb58eac6a
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-09-15 21:08:20 +0530

    checkinh user authentication before connecting with server

--- Project3/src/sockets/socket.server.js
+++ Project3/src/sockets/socket.server.js
@@ -1,9 +1,26 @@
 const {Server}  =  require("socket.io");
 // import { init } from "../models/chat.models";
+const cookie = require("cookie")
 
 function initSocketServer(httpServer){
 
     const io = new Server(httpServer,{})
+
+    io.use((socket,next)=>{
+
+        const cookies = cookie.parse(socket.handshake.headers.cookie);
+        console.log("Socket connection cooies :", cookies);
+
+        if(!cookies.token){
+            res.status(401).json({
+                message : "no user found"
+            })
+        }
+        
+
+        next();
+        
+    })
 
     io.on("connection", (socket)=>{
         console.log("New socket connection : " , socket.id);
