26cbeb83d5b6002f829262a75729daf3fe50abdb
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-09-15 21:31:29 +0530

    socket.io middleware created

--- Project3/src/sockets/socket.server.js
+++ Project3/src/sockets/socket.server.js
@@ -1,27 +1,49 @@
 const {Server}  =  require("socket.io");
 // import { init } from "../models/chat.models";
 const cookie = require("cookie")
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/user.models")
 
 function initSocketServer(httpServer){
 
     const io = new Server(httpServer,{})
 
-    io.use((socket,next)=>{
 
-        const cookies = cookie.parse(socket.handshake.headers.cookie);
+    // socket.io middleware 
+    io.use(async (socket,next)=>{
+
+        const cookies = cookie.parse(socket.handshake.headers.cookie || ""); 
+        /** code to extract value of cookie passed at postman header side */
+
+
         console.log("Socket connection cooies :", cookies);
 
         if(!cookies.token){
-            res.status(401).json({
-                message : "no user found"
-            })
+            next(new Error("Authentication error : no token provider"));
         }
-        
+
+        try {
+            const decode = jwt.verify(cookies.token, process.env.JWT_TOKEN)
+
+            const user = await userModel.findById(decode.id)
+
+            socket.user = user;
+
+            next();
+
+
+        } catch (error) {
+            next(new Error("Authentication error : Invalid Token"))
+        }
+
 
         next();
         
     })
 
+
+
+    // socket.io starting server
     io.on("connection", (socket)=>{
         console.log("New socket connection : " , socket.id);
         
