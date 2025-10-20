d90dc0fced6d354950698582597cf04bcc9e470e
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-10-20 19:29:00 +0530

    Done with the Login and register pages

--- Project3/backend/src/app.js
+++ Project3/backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
 
 
 /* Routes */
@@ -10,6 +11,10 @@ const chatRoutes = require("./routes/chat.routes")
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 /* Using Routes */
 app.use('/auth',authRoutes)
