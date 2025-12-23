cb62e47943ff91f2e2c95f0c0b1c0600ce6f673d
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-12-24 02:50:14 +0530

    implementing LLM response using our MCP's tools

--- MCP/mcp.client.js
+++ MCP/mcp.client.js
@@ -1,41 +1,13 @@
+import { config } from "dotenv";    
 import { GoogleGenAI , Type } from "@google/genai";
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
-
+config();
 
 const tools = [];
 
-const scheduleMeetingFunctionDeclaration = {
-  name: 'schedule_meeting',
-  description: 'Schedules a meeting with specified attendees at a given time and date.',
-  parameters: {
-    type: Type.OBJECT,
-    properties: {
-      attendees: {
-        type: Type.ARRAY,
-        items: { type: Type.STRING },
-        description: 'List of people attending the meeting.',
-      },
-      date: {
-        type: Type.STRING,
-        description: 'Date of the meeting (e.g., "2024-07-29")',
-      },
-      time: {
-        type: Type.STRING,
-        description: 'Time of the meeting (e.g., "15:00")',
-      },
-      topic: {
-        type: Type.STRING,
-        description: 'The subject or topic of the meeting.',
-      },
-    },
-    required: ['attendees', 'date', 'time', 'topic'],
-  },
-};
-
 const ai = new GoogleGenAI({
-    model : "gemini-2.0-flash",
     apiKey : process.env.GOOGLE_API_KEY || "",
 })
 
@@ -53,7 +25,9 @@ const client = new Client({
 await client.connect(transport);
 
 
-client.listTools().then(response=>{
+client.listTools().then(async response=>{
+
+
     response.tools.forEach(tool=>{
         tools.push({
             name : tool.name,
@@ -66,7 +40,27 @@ client.listTools().then(response=>{
         });
     })
 
-    console.log("registration Tools :", tools);
+    const aiResponse = await ai.models.generateContent({
+        model: 'gemini-2.0-flash',
+        contents: 'add 2 and 3',
+        config: {
+            tools: [{
+                functionDeclarations: tools
+            }],
+        },
+    });
+
+    console.log("AI Response : ", aiResponse.functionCalls);
+
+    aiResponse.functionCalls.forEach(async call =>{
+        const toolResponse = await client.callTool({
+            name : call.name,
+            arguments : call.args
+        })
+        console.log("Tools Response : ", toolResponse);
+        
+    })
+    
     
     
 })
