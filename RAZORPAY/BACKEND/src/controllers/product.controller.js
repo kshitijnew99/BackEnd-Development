a880efc02622677e2e037832b109fc26748fa6ce
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-12-25 00:34:13 +0530

    setting the product controller

--- RAZORPAY/BACKEND/src/controllers/product.controller.js
+++ RAZORPAY/BACKEND/src/controllers/product.controller.js
@@ -3,5 +3,24 @@ const productModel = require('../models/product.model');
 
 
 async function createProduct(req, res) {
-    
+    const {image, title , description , category , price : {amount , currency}} = req.body;
+
+    try {
+
+        const product = new productModel.create({
+            image , title , description , category , price : {amount , currency}
+        })
+
+        return res.status(201).send({
+            message : "Product created successfully",
+            CreatedProduct : product
+        })
+    } catch (err){
+        return res.status(500).send({
+            message : "Error creating product",
+            error : err.message
+        });
+    }
 }
+
+module.exports = createProduct;
