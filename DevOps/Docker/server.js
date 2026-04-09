97a75f634967c45433d22b741e2e9061c02457f7
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2026-04-10 03:07:22 +0530

    Docker

---
+++ DevOps/Docker/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message : "Hello, World! I'm running in a Docker container."
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+    
+})
